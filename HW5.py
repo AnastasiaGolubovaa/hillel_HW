729f4abe2a57fec1e6b8d9930f79b52d74aebc4f
Anastasia <anastasia.golubovaa@gmail.com>
2022-12-22 09:35:46 +0200

    Home work-5

--- HW5.py
+++ HW5.py
@@ -1,16 +1,41 @@
 # 1)
-for N in "*****","****","***","**","*":
-    print(N)
+N =5
+for i in range(N + 1, 0, -1):
+    for j in range(0, i - 1):
+        print("*", end=' ')
+    print(" ")
 
-# 2)
-for N in "*","**","***","****","*****":
-    print(N)
+#2
+N = 5
+myList = []
+for i in range(1, N + 1):
+    myList.append("*" * i)
+print("\n".join(myList))
+
+
+#OR
+
+N = 5
+for i in range(0, N):
+    for j in range(0, i + 1):
+        print("* ", end="")
+    print()
 
 # Задание со звездочкой:
-# 3)
-for N in "*****"," ****","  ***","   **","    *":
-    print(N)
 
-# 4)
-for N in "    *","   **","  ***"," ****","*****":
-    print(N)
+#3
+N = 5
+for i in range(0, N):
+    for j in range(0, i):
+        print(' ', end=' ')
+    for j in range(0, N - i):
+        print('*', end=' ')
+    print('')
+print()
+
+# 4
+
+N = 5
+for i in range(1, N + 1):
+	print(" " * (N - i) +"*" * i)
+
