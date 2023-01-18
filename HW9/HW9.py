766d311e438101601ab8a9ce848239f9e8da206c
Anastasia <anastasia.golubovaa@gmail.com>
2023-01-18 19:45:53 +0200

    structured files (home works)

---
+++ HW9/HW9.py
@@ -0,0 +1,19 @@
+#1 !!!
+# with open("numbers.txt") as f:
+#     numbers = map(lambda x:(x.replace("\n", "")), f.readlines())
+#
+#     filter(numbers.isdigit, f.readlines())
+#     print(numbers)
+
+#2
+# text = input("Enter your text: ")
+# open("text2.txt", "w").write(text)
+
+#3 !!!
+# N = int(input("Enter your number:"))
+# file = open(numbers.txt, )
+
+#4
+#5
+#6
+#7
