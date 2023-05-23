d4dfb994fdd5e13cf038057e57b1ce87f2fb72ee
AnastasiaGolubova <nastia.g1355@gmail.com>
2023-05-23 14:37:33 +0300

    HW12.2version

--- HW12/HW12.py
+++ HW12/HW12.py
@@ -1,15 +1,18 @@
 def is_power_of_two(number):
-    if number <= 0:
-        return "no"
-    elif number == 1:
-        return "yes"
+    if number == 1:
+        return 'yes'
+    elif number % 2 == 1 or number == 0:
+        return 'no'
     else:
-        while number % 2 == 0:
-            number //= 2
-        return "yes" if number == 1 else "no"
+        return is_power_of_two(number // 2)
+
 print(is_power_of_two(256))
 print(is_power_of_two(12))
 print(is_power_of_two(16))
+
+
+
+
 
 
 
