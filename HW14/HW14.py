64f1a705961e080a9d124a01cefd66f6ae6c488c
AnastasiaGolubova <nastia.g1355@gmail.com>
2023-06-04 13:13:58 +0300

    HW14

---
+++ HW14/HW14.py
@@ -0,0 +1,24 @@
+from abc import ABC, abstractmethod
+class Shape(ABC):
+    @abstractmethod
+    def draw(self) -> None:
+        pass
+class Triangle(Shape):
+    def __init__(self, width: int):
+        self.width = width
+    def draw(self) -> None:
+        for i in range(1, self.width + 1):
+            print('*' * i)
+class Rectangle(Shape):
+    def __init__(self, width: int, height: int):
+        self.width = width
+        self.height = height
+    def draw(self) -> None:
+        for _ in range(self.height):
+            print('*' * self.width)
+
+shapes = [Triangle(5), Rectangle(4, 3)]
+
+for shape in shapes:
+    shape.draw()
+    print()
